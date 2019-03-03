85f9acb77fd4c2b5b559cc22545163ac8b4e85c2
Karishma Vanjani <karishmavanjani@Karishmas-MBP.fios-router.home>
2019-03-02 20:51:28 -0500

    final project

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+	// $("#commute").on("click", function (e) {
+	e.preventDefault(); //disable the button's default behavior
+	if (type == "number" > 5) { alert("NOT WORTH IT") }
+	// getNumber1 = $("#number1"),
+	// num1 = parseFloat(getNumber1.val()); //store the number of input 1
+	else {
+		alert("WORTH IT")
+	}
+})
